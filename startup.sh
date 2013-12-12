96314d46d14cec959aa5eec90e71e367e4a08535
Laird Liu <qiao.liu@yottaa.com>
2013-12-12 09:16:27 +0800

    edit Dockerfile

--- startup.sh
+++ startup.sh
@@ -21,22 +21,6 @@ cd /src; tar -C /home/docker -xvf config.tar
 chown -R docker:docker /home/docker/*
 
 
-if [ -d "/home/docker/.rbenv" ]; then
-	echo "installed !!!"
-else
-	su - docker 
-	echo "Su docker user !!!!"
-	git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-	echo "export PATH=\"$HOME/.rbenv/bin:$PATH\"" >> ~/.bashrc
-	echo "eval \"\$(rbenv init -)\"" >> ~/.bashrc
-
-	git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-	source ~/.bashrc
-	rbenv install 2.0.0-p247
-	rbenv rehash
-	rbenv global 2.0.0-p247
-fi
 
 # restarts the xdm service
 /etc/init.d/xdm restart
